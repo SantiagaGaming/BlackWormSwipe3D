599465f084659c287d6b857c9c47d28ab9ef8116
Ilya <ganychev92@gmail.com>
2022-03-18 12:03:35 +0300

    Level Mover Added

---
+++ Assets/Scripts/LevelMover.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMover : MonoBehaviour
+{
+   [SerializeField] private float _speed;
+
+    void Update()
+    {
+        transform.position += Vector3.up * _speed * Time.deltaTime;
+    }
+}
